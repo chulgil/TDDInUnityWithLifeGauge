c2e263d5227e458f4f2526996e003a0e66f85886
CG.Lee <contact@chulgil.me>
2019-06-14 15:41:53 +0900

    update unity resouces

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -4,19 +4,22 @@ using UnityEngine.UI;
 public class App : MonoBehaviour
 {
     [SerializeField] private Image m_image;
+    private Heart m_heart;
+    
+    private void Start() 
+    {
+        m_heart = new Heart(m_image);
+    }
 
     private void Update() 
     {
-        var heart = new Heart(m_image);
-
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            heart.Replenish(1);
+            m_heart.Replenish(1);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            heart.Deplete(1);
+            m_heart.Deplete(1);
         }
-
     }
 }
