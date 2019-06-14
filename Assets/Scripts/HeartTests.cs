2d85132861a55d0a9cff3542f62db79337cdc43c
CG.Lee <contact@chulgil.me>
2019-06-14 14:04:52 +0900

    refactoring HeartTests

--- Assets/Scripts/HeartTests.cs
+++ Assets/Scripts/HeartTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
@@ -9,43 +10,69 @@ namespace Tests
 {
     public class HeartTests : MonoBehaviour
     {
+
+        #region TheReplenishMethod
         public class TheReplenishMethod
         {
+            private Image m_image;
+            private Heart m_heart;
+
+            [SetUp]
+            public void BeforeEveryTest()
+            {
+                m_image = new GameObject().AddComponent<Image>();
+                m_heart = new Heart(m_image);
+            }
+        
+
             [Test]
             public void _0_Sets_Image_With_0_Fill_To_0_Fill()
             {
-                var image = new GameObject().AddComponent<Image>();
-                image.fillAmount = 0;
-                var heart = new Heart(image);
+                m_image.fillAmount = 0;
 
-                heart.Replenish(0);
-                Assert.AreEqual(0, image.fillAmount);
+                m_heart.Replenish(0);
+
+                Assert.AreEqual(0, m_image.fillAmount);
             }
 
             [Test]
             public void _1_Sets_Image_With_0_Fill_To_25_Percent_Fill()
             {
-                    var image = new GameObject().AddComponent<Image>();
-                    image.fillAmount = 0;
-                    var heart = new Heart(image);
+                    m_image.fillAmount = 0;
 
-                    heart.Replenish(1);
-                    Assert.AreEqual(0.25f, image.fillAmount);            
+                    m_heart.Replenish(1);
+
+                    Assert.AreEqual(0.25f, m_image.fillAmount);            
             }
 
             [Test]
             public void _1_Sets_Image_With_25_Percent_Fill_To_50_Percent_Fill()
             {
-                    var image = new GameObject().AddComponent<Image>();
-                    image.fillAmount = 0.25f;
-                    var heart = new Heart(image);
+                    m_image.fillAmount = 0.25f;
 
-                    heart.Replenish(1);
-                    Assert.AreEqual(0.5f, image.fillAmount);            
+                    m_heart.Replenish(1);
+                    
+                    Assert.AreEqual(0.5f, m_image.fillAmount);            
             }                  
 
         }
-       
+        #endregion
+
+        #region TheDepleteMethod
+        public class TheDepleteMethod
+        {
+            [Test]
+            public void _0_Sets_Image_With_1_Fill_To_1_Fill()
+            {
+                var image = new GameObject().AddComponent<Image>();
+                var heart = new Heart(image);
+                heart.Deplete(0);
+
+                Assert.AreEqual(1, image.fillAmount);
+            }
+
+        }
+        #endregion
 
     }
     public class Heart
@@ -61,6 +88,12 @@ namespace Tests
         public void Replenish(int numberOfHeartPieces)
         {
             m_image.fillAmount += numberOfHeartPieces* FillPerHeartPercentage;
+        }
+
+        public void Deplete(int numberOfHeartPieces)
+        {
+
+            throw new NotImplementedException();
         }
     }    
 }
