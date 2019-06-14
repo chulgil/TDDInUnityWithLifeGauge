2f651ee4460784e92d75b5d1562fb7bac90a9418
CG.Lee <contact@chulgil.me>
2019-06-14 15:29:37 +0900

    create App class

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class App : MonoBehaviour
+{
+    [SerializeField] private Image m_image;
+
+    private void Update() 
+    {
+        var heart = new Heart(m_image);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            heart.Replenish(1);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            heart.Deplete(1);
+        }
+
+    }
+}
