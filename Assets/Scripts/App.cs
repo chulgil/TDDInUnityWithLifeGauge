600175948cc75f7fe439b83688305b2d6e95dab3
CG.Lee <contact@chulgil.me>
2019-06-17 13:07:54 +0900

    Update scene for 4 hearts container

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -1,25 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 public class App : MonoBehaviour
 {
-    [SerializeField] private Image m_image;
-    private Heart m_heart;
+    [SerializeField] private List<Image> m_images;
+    [SerializeField] private int m_amount;
+    private HeartContainer m_heartContainer;
     
     private void Start() 
     {
-        m_heart = new Heart(m_image);
+        m_heartContainer = new HeartContainer(
+            m_images.Select(image => new Heart(image)).ToList());
     }
 
     private void Update() 
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            m_heart.Replenish(1);
+            m_heartContainer.Replenish(m_amount);
+            Debug.Log("UpArrow : " + m_amount.ToString());
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            m_heart.Deplete(1);
+            m_heartContainer.Deplete(m_amount);
+            Debug.Log("DownArrow : " + m_amount.ToString());
+
         }
     }
 }
